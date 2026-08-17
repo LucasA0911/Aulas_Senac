20743b15b9bddf5c117bfb1f48331cd0de78b07c
DataBard0911 <badboy12121998@gmail.com>
2026-08-17 17:03:58 -0300

    Exercicios para casa

---
+++ Algoritimos e Programação para IA/Exercicios/Ex02_Lucas_Alves_Dos_Santos.py
@@ -0,0 +1,17 @@
+nome_aluno = "lucas_alves_dos_santos"
+print(f"O nome do aluno é: {nome_aluno}, e o tipo da variável é: {type(nome_aluno).__name__}")
+
+print(f"O tipo da variável nome_aluno é: {type(nome_aluno).__name__}")
+
+b_valor = True
+print(f"O tipo da variável b_valor é: {type(b_valor).__name__}")
+
+idade = 27
+print(f"O tipo da variável idade é: {type(idade).__name__}")
+
+altura = 1.65
+print(f"O tipo da variável altura é: {type(altura).__name__}")
+
+aceno = "Olá"
+print(aceno + " Algoritmos para IA")
+
